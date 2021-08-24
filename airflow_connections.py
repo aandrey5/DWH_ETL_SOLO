6603df35610d0a99caef799cffb7b48919c0133e
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-08-24 17:09:23 +0300

    Update airflow_connections.py

--- airflow_connections.py
+++ airflow_connections.py
@@ -1,1 +1,36 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+import json
+import os
+import datetime as dt
+import pandas as pd
+import numpy as np
+from airflow.models import Variable
+from airflow.hooks.base_hook import BaseHook
+from sqlalchemy import create_engine, String, Integer, Float
+from time import sleep
+import logging
+import psycopg2
 
+connection_input = BaseHook.get_connection("airflow_connection")
+
+
+@dag(default_args={'owner': 'MorozovAN'}, 
+  schedule_interval=None,
+  tags=['ds'], 
+  start_date=days_ago(0))
+
+def Task10_test():
+
+  @task()
+  def test_connection():
+
+    logging.info(f'postgresql://{connection_input.login}:{connection_input.password}@10.128.100.98/dwh')
+    return 1
+
+
+  test_connection()
+
+        #sleep(1)
+
+Taskflow = Task10_test()
